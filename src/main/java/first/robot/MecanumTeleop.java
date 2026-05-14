8ac07fb6fe58c5ab04592b5e7a64531be835689a
Miriam Sinton-Remes <miriamsintonremes@gmail.com>
2026-05-13 20:10:25 -0400

    Applied a deadband to each joystick

--- src/main/java/first/robot/MecanumTeleop.java
+++ src/main/java/first/robot/MecanumTeleop.java
@@ -1,6 +1,7 @@
 package first.robot;
 
 import org.wpilib.driverstation.Gamepad;
+import org.wpilib.math.util.MathUtil;
 import org.wpilib.opmode.PeriodicOpMode;
 import org.wpilib.opmode.Teleop;
 
@@ -9,6 +10,7 @@ public class MecanumTeleop extends PeriodicOpMode {
   private final Robot robot;
   private final Gamepad gamepad = new Gamepad(0);
   private final float MAX_SPEED = 0.5f;
+  private final float DEADBAND = 0.1f;
 
   /** The Robot instance is passed into the opmode via the constructor. */
   public MecanumTeleop(Robot robot) {
@@ -18,9 +20,9 @@ public class MecanumTeleop extends PeriodicOpMode {
   @Override
   public void periodic() {
     robot.setDrivePowers(
-      -gamepad.getLeftY() * MAX_SPEED,
-      gamepad.getLeftX() * MAX_SPEED,
-      gamepad.getRightX() * MAX_SPEED
+      -MathUtil.applyDeadband(gamepad.getLeftY(), DEADBAND) * MAX_SPEED,
+      MathUtil.applyDeadband(gamepad.getLeftX(), DEADBAND) * MAX_SPEED,
+      MathUtil.applyDeadband(gamepad.getRightX(), DEADBAND) * MAX_SPEED
     );
   }
 }
