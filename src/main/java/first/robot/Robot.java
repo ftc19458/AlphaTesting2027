67053bf901fd56a3c3595f8636a4d444726a6479
Isaac Sinton-Remes <chaosleader125@gmail.com>
2026-05-16 15:22:36 -0400

    Tried  to get a simple forward movement working

--- src/main/java/first/robot/Robot.java
+++ src/main/java/first/robot/Robot.java
@@ -11,6 +11,7 @@ public class Robot extends OpModeRobot {
   private final ExpansionHubMotor backRight = new ExpansionHubMotor(0, 2);
   private final ExpansionHubMotor frontRight = new ExpansionHubMotor(0, 3);
 
+
   public final MecanumDrive drive;
 
   /**
@@ -20,16 +21,11 @@ public class Robot extends OpModeRobot {
   public Robot() {
     if (isAutonomous()) {
       resetEncoders();
-      drive = new MecanumDrive(
-        frontLeft::setPositionSetpoint,
-        backLeft::setPositionSetpoint,
-        frontRight::setPositionSetpoint,
-        backRight::setPositionSetpoint
-      );
-      frontLeft.getVelocityConstants().setPID(0.1, 0, 0);
-      backLeft.getVelocityConstants().setPID(0.1, 0, 0);
-      frontRight.getVelocityConstants().setPID(0.1, 0, 0);
-      backRight.getVelocityConstants().setPID(0.1, 0, 0);
+      drive = null;
+      frontLeft.getPositionConstants().setPID(0.3, 0,0);
+      backLeft.getPositionConstants().setPID(0.3, 0, 0);
+      frontRight.getPositionConstants().setPID(0.3, 0, 0);
+      backRight.getPositionConstants().setPID(0.3, 0, 0);
     } else {
       drive = new MecanumDrive(
         frontLeft::setThrottle,
@@ -51,23 +47,38 @@ public class Robot extends OpModeRobot {
 
   public void setDrivePowers(double x, double y, double rotation) {
     drive.driveCartesian(x, y, rotation);
+    
+  }
+
+  //temporary using the drive encodrs until I setup odo
+  public void setSetpoints(double x, double y){
+    frontLeft.setPositionSetpoint(x + y);
+    backLeft.setPositionSetpoint(x - y);
+    frontRight.setPositionSetpoint(x - y );
+    backRight.setPositionSetpoint(x + y);
+
   }
 
   public boolean isDoneMoving(int target, int tolerance) {
-    double flPos = frontLeft.getEncoderPosition();
-    double blPos = backLeft.getEncoderPosition();
-    double frPos = frontRight.getEncoderPosition();
-    double brPos = backRight.getEncoderPosition();
 
-    SmartDashboard.putNumber("front left", flPos);
-    SmartDashboard.putNumber("back left", blPos);
-    SmartDashboard.putNumber("front right", frPos);
-    SmartDashboard.putNumber("back right", brPos);
 
     return Math.abs(Math.abs(frontLeft.getEncoderPosition()) - target) <= tolerance &&
            Math.abs(Math.abs(backLeft.getEncoderPosition()) - target) <= tolerance &&
            Math.abs(Math.abs(frontRight.getEncoderPosition()) - target) <= tolerance &&
            Math.abs(Math.abs(backRight.getEncoderPosition()) - target) <= tolerance;
+  }
+
+  public void  updateDash(){
+    double flPos = frontLeft.getEncoderPosition();
+    double blPos = backLeft.getEncoderPosition();
+    double frPos = frontRight.getEncoderPosition();
+    double brPos = backRight.getEncoderPosition();
+
+
+    SmartDashboard.putNumber("front left", flPos);
+    SmartDashboard.putNumber("back left", blPos);
+    SmartDashboard.putNumber("front right", frPos);
+    SmartDashboard.putNumber("back right", brPos);
   }
 
   public void resetEncoders() {
---
+++ src/main/java/first/robot/SimpleAuto.java
@@ -0,0 +1,27 @@
+package first.robot;
+
+import org.wpilib.opmode.Autonomous;
+import org.wpilib.opmode.PeriodicOpMode;
+
+@Autonomous
+public class SimpleAuto extends PeriodicOpMode {
+    private final Robot robot;
+    private final int MOVMENT_SIZE = 200; // ticks
+
+    public SimpleAuto(Robot robot) {
+        this.robot = robot;
+    }
+
+
+
+    @Override
+    public void periodic() {
+        robot.updateDash();
+    }
+
+    @Override
+    public void start() {
+        robot.resetEncoders();
+        robot.setSetpoints(MOVMENT_SIZE, 0);
+    }
+}
